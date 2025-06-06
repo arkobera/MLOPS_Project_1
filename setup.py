b3fb9f1b8b7c50992c922a73456f03ed061300bf
arko <arkobera77@gmail.com>
2025-06-06 21:02:35 +0530

    Initial Project Template

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arko Bera",
+    author_email="arkobera@example.com",
+    packages=find_packages()
+)
